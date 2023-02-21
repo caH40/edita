9f0c9b6c762725df41ebd161c42c58d11452fc85
Alexander Berezhnev <freelancerbav@gmail.com>
2023-02-21 17:37:23 +0300

    added a slider

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+import { Carousel } from 'https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.esm.js';
+
+const myCarousel = new Carousel(document.querySelector('#myCarousel'), { Dots: false });
+
+const leftArrow = document.querySelector('.promo-main__control-left');
+const rightArrow = document.querySelector('.promo-main__control-right');
+
+rightArrow.onclick = myCarousel.slideNext;
+leftArrow.onclick = myCarousel.slidePrev;
