647a501bb1384ce997bc66c7886b0278a9301649
Alexander Berezhnev <freelancerbav@gmail.com>
2023-02-22 17:21:06 +0300

    fixed the promo-slider

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 import { Carousel } from 'https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.esm.js';
 
-const myCarousel = new Carousel(document.querySelector('#myCarousel'), { Dots: false });
+const myCarousel = new Carousel(document.querySelector('.promo__carousel'), { Dots: false });
 
 const leftArrow = document.querySelector('.promo-main__control-left');
 const rightArrow = document.querySelector('.promo-main__control-right');
